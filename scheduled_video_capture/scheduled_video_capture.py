00ddb6b8e7bae966386c4383f8bb8c87144ef3a3
Drew H <dhardie@clearblade.com>
2025-03-06 11:53:25 -0600

    cleaner time unit implementation

--- scheduled_video_capture/scheduled_video_capture.py
+++ scheduled_video_capture/scheduled_video_capture.py
@@ -3,6 +3,12 @@ from datetime import datetime, timezone
 import cv2
 import dateutil.parser as parser
 
+time_units = {
+    "Minutes": 60,
+    "Hours": 3600,
+    "Days": 86400
+}
+
 #Class to manage video capture sessions for multiple cameras
 class VideoCaptureSessions:
     def __init__(self):
@@ -164,18 +170,12 @@ def save_frame(frame, camera_id, task_uuid, task_settings, task_id):
     resolution = task_settings.get("resolution", "Low")
     interval = int(task_settings.get("interval", 3600))
     interval_units = task_settings.get("interval_units", "Seconds")
-    if interval_units == "Minutes":
-        interval *= 60
-    elif interval_units == "Hours":
-        interval *= 3600
-    elif interval_units == "Days":
-        interval *= 86400
+    if interval_units != "Seconds":
+        interval *= time_units.get(interval_units, 1)
     duration = int(task_settings.get("clip_length", interval))
     duration_units = task_settings.get("clip_length_units", "Seconds")
-    if duration_units == "Minutes":
-        duration *= 60
-    elif duration_units == "Hours":
-        duration *= 3600
+    if duration_units != "Seconds":
+        duration *= time_units.get(duration_units, 1)
     start_time = task_settings.get("start_time", datetime.now(timezone.utc).isoformat())
     timestamp = datetime.now(timezone.utc).isoformat()
    
